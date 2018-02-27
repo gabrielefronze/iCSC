3d200bfb4be58024b4e71597cbaea6881157c349
Gabriele Gaetano Fronzé <gabriele.gaetano.fronze@cern.ch>
2018-02-27 15:41:11 +0100

    Update of basic num_steps

--- common.h
+++ common.h
@@ -1,7 +1,7 @@
 #include <iostream>
 
 namespace common {
-    const unsigned long long num_steps = 500000000; //number of x bins
+    const unsigned long long num_steps = 5000000; //number of x bins
 
     void print_results(double pi, int n_threads){
             printf("Pi value: %f\n Number of steps: %llu\n Number of threads: %d\n",pi,num_steps,n_threads);
